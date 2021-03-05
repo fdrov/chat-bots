d973ab26f97a4cef49b1f0f4d5229db252e8e315
Fdrv <oldgot@gmail.com>
2021-03-05 20:51:50 +0300

    Delete prints

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ url = 'https://dvmn.org/api/long_polling/'
 
 def check_dvmn(response):
     if response.json()["status"] == "found":
-        print(response.json()["new_attempts"])
         message = 'К сожалению, в работе нашлись ошибки' if response.json()["new_attempts"][0][
             "is_negative"] else 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
         lesson_url = parse.urljoin(response.url, response.json()["new_attempts"][0]["lesson_url"])
@@ -23,18 +22,15 @@ def check_dvmn(response):
                          text=f'У вас проверили работу «{response.json()["new_attempts"][0]["lesson_title"]}»\n' + message + lesson_url)
         timestamp = response.json()["last_attempt_timestamp"]
     elif response.json()["status"] == "timeout":
-        print("status: timeout")
         timestamp = response.json()["timestamp_to_request"]
 
 
 while True:
     try:
         response = requests.get(url, headers=headers, timeout=90)
-    except requests.exceptions.ReadTimeout as err:
-        print(err)
+    except requests.exceptions.ReadTimeout:
         continue
-    except requests.exceptions.ConnectionError as err:
-        print(err)
+    except requests.exceptions.ConnectionError:
         sleep(10)
         continue
     else:
