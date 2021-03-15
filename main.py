ff23bef6a37f94a5ec1a5c2b55a99ba41a1703b5
Fdrv <oldgot@gmail.com>
2021-03-15 21:25:31 +0300

    Fix logger, some minor code-style refactor

--- main.py
+++ main.py
@@ -7,12 +7,21 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-format = "%(asctime)-25s %(levelname)s. func:%(funcName)s %(message)s"
-# logging.basicConfig(filename='app.log', filemode='w', level=logging.DEBUG, format=format)
-logging.basicConfig(level=logging.DEBUG, format=format)
+logger = logging.getLogger('chat_log')
 
 
 def main():
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('{asctime} {levelname}. func:{funcName} {message}', style='{')
+
+    c_handler = logging.StreamHandler()
+    c_handler.setFormatter(formatter)
+    logger.addHandler(c_handler)
+
+    f_handler = logging.FileHandler('chat.log', mode='w')
+    f_handler.setFormatter(formatter)
+    logger.addHandler(f_handler)
+
     load_dotenv()
     headers = {'Authorization': os.getenv('DVMN_TOKEN')}
     url = 'https://dvmn.org/api/long_polling/'
@@ -28,10 +37,12 @@ def handle_dvmn_response(response):
         attempts = response_dict["new_attempts"]
         for attempt in attempts:
             greeting = f'У вас проверили работу «{attempt["lesson_title"]}»\n'
-            status = 'К сожалению, в работе нашлись ошибки' if attempt[
-                "is_negative"] else 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
+            status = ('К сожалению, в работе нашлись ошибки'
+                      if attempt["is_negative"]
+                      else 'Преподавателю всё понравилось, можно приступать к следующему уроку!')
             lesson_url = parse.urljoin(response.url, attempt["lesson_url"])
-            bot.send_message(chat_id=os.getenv('CHAT_ID'), text=greeting + status + lesson_url)
+            msg = f'{greeting}\n{status}\n{lesson_url}'
+            bot.send_message(chat_id=os.getenv('CHAT_ID'), text=msg)
             timestamp_to_request = response_dict["last_attempt_timestamp"]
     elif response_dict["status"] == "timeout":
         timestamp_to_request = response_dict["timestamp_to_request"]
@@ -40,22 +51,22 @@ def handle_dvmn_response(response):
 
 def handle_connection(url, headers):
     payload = {'timestamp': None}
-    logging.info('Начинаю соединение')
+    logger.warning('Начинаю соединение')
     while True:
-        logging.debug('Начинаю while-loop')
+        logger.debug('Начинаю while-loop')
         try:
             response = requests.get(url, headers=headers, params=payload, timeout=5)
-            logging.debug(f'Запрашиваемый адрес: {response.url}')
+            logger.debug(f'Запрашиваемый адрес: {response.url}')
             response.raise_for_status()
         except requests.exceptions.ReadTimeout:
-            logging.debug('TIMEOUT')
+            logger.debug('TIMEOUT')
             continue
         except requests.exceptions.ConnectionError:
-            logging.error('ConnectionError. Try to reconnect. You have 10 set to the next try.')
+            logger.error('ConnectionError. Try to reconnect. You have 10 set to the next try.')
             sleep(10)
             continue
         except requests.exceptions.HTTPError:
-            logging.debug(f'Код ответа {response.status_code}')
+            logger.debug(f'Код ответа {response.status_code}')
             if 400 <= response.status_code <= 402:
                 print('Проверьте правильность токена сайта dvmn.org')
             elif response.status_code == 404:
@@ -65,9 +76,9 @@ def handle_connection(url, headers):
             print('Программа заканчивает работу')
             break
         else:
-            logging.info('Сайт dvmn ответил')
+            logger.info('Сайт dvmn ответил')
             payload['timestamp'] = handle_dvmn_response(response)
-            logging.debug(f'Установлен новый timestamp {payload["timestamp"]}')
+            logger.debug(f'Установлен новый timestamp {payload["timestamp"]}')
 
 
 if __name__ == '__main__':
